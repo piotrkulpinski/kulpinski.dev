87e920f759498311a6284c9cffe61cb71d5f0746
Piotr Kulpinski <piotr@kulpinski.pl>
2024-03-28 17:02:55 +0100

    add breadcrumbs guide in remix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,16 @@
 import { defineCollection, z } from "astro:content"
 
 const postsCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string().optional(),
-    author: z.string().optional(),
-
-    image: z
-      .object({
-        url: z.string(),
-        alt: z.string(),
-      })
-      .optional(),
-
-    tags: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string().optional(),
+      author: z.string().optional(),
+      image: image().optional(),
+      ogImage: image().optional(),
+      tags: z.array(z.string()),
+    }),
 })
 
 const projectsCollection = defineCollection({
