1da2d580f522ff1e3b1cc7a7c8f6a30d30bf7419
Piotr Kulpinski <piotr@kulpinski.pl>
2024-03-28 23:17:27 +0100

    fix issues with resvg

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,15 @@ import cloudflare from "@astrojs/cloudflare"
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  compressHTML: true,
   adapter: cloudflare({
     imageServiceSection: "cloudflare",
   }),
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
   markdown: {
     drafts: true,
     shikiConfig: {
@@ -23,6 +29,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true,
   },
-  site: "https://kulpinski.dev",
-  integrations: [tailwind(), sitemap(), mdx(), preact()],
+  site: process.env.SITE_URL || "http://localhost:4321",
+  integrations: [tailwind({ applyBaseStyles: false }), sitemap(), mdx(), preact()],
 })
