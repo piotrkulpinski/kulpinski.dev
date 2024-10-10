b17a7cc2ae88235a4265466b4796a53abce4a09a
Piotr Kulpinski <piotr@kulpinski.pl>
2024-10-10 13:13:18 +0200

    Fix trailing slash never in production builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import rehypeExternalLinks from "rehype-external-links"
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import sitemap from "@astrojs/sitemap"
 import mdx from "@astrojs/mdx"
 import preact from "@astrojs/preact"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+import rehypeExternalLinks from "rehype-external-links"
 
 import cloudflare from "@astrojs/cloudflare"
 
@@ -12,6 +12,9 @@ export default defineConfig({
   output: "hybrid",
   compressHTML: true,
   trailingSlash: "never",
+  build: {
+    format: "file", // Fix trailing slash never in production builds
+  },
   adapter: cloudflare({
     imageServiceSection: "cloudflare",
   }),
