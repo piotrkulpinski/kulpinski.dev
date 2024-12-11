1656e1c0be2bb3615fe99d631fd23c3813668737
Piotr Kulpinski <piotr@kulpinski.pl>
2024-12-11 16:32:40 +0100

    remove unnecessary astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
 import cloudflare from "@astrojs/cloudflare"
 import mdx from "@astrojs/mdx"
+import preact from "@astrojs/preact"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
 import rehypeExternalLinks from "rehype-external-links"
-
-import preact from "@astrojs/preact"
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   adapter: cloudflare({
     imageServiceSection: "cloudflare",
-    platformProxy: {
-      enabled: true,
-    },
   }),
   markdown: {
     drafts: true,
@@ -26,7 +22,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true,
   },
-  vite: { ssr: { external: ["node:buffer"] } },
   site: process.env.SITE_URL || "http://localhost:4321",
   integrations: [tailwind({ applyBaseStyles: false }), preact(), sitemap(), mdx()],
 })
